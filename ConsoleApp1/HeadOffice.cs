0a76272b6bd04c52f0b937f5612325f527960c2a
bashir0vrashad <beshirovreshad777@gmail.com>
2023-12-04 19:27:21 +0400

    chain of res

---
+++ ConsoleApp1/HeadOffice.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public class HeadOffice : Handler
+    {
+        public override void HandleRequest(int amount)
+        {
+            if (amount > 50000)
+            {
+                Console.WriteLine($"{this.GetType().Name} handles the loan request for {amount:C}");
+            }
+            else if (successor != null)
+            {
+                successor.HandleRequest(amount);
+            }
+            else
+            {
+                Console.WriteLine("Loan request cannot be handled.");
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿using ConsoleApp1;
+
+Handler bankManager = new BankManager();
+Handler regionalManager = new RegionalManager();
+Handler headOffice = new HeadOffice();
+
+bankManager.SetSuccessor(regionalManager);
+regionalManager.SetSuccessor(headOffice);
+
+
+bankManager.HandleRequest(8000);
+bankManager.HandleRequest(25000);
+bankManager.HandleRequest(70000);
